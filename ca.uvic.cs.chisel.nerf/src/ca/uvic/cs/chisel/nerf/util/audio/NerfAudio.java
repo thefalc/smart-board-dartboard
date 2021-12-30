6a29793045a3266da6d6ac48b88eb1f17daa851d
Sean Falconer <sfalc@google.com>
2021-12-30 06:47:41 -0800

    Cleaned up file headers

--- ca.uvic.cs.chisel.nerf/src/ca/uvic/cs/chisel/nerf/util/audio/NerfAudio.java
+++ ca.uvic.cs.chisel.nerf/src/ca/uvic/cs/chisel/nerf/util/audio/NerfAudio.java
@@ -1,7 +1,3 @@
-/**
- * Copyright 1998-2007, CHISEL Group, University of Victoria, Victoria, BC, Canada.
- * All rights reserved.
- */
 package ca.uvic.cs.chisel.nerf.util.audio;
 
 import java.io.File;
